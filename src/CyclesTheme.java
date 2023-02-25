d96794641c2ee2df0843f02715fe62f3a1943f99
chekmenev94 <andrei_94_chekm@mail.ru>
2023-02-25 17:36:56 +0300

    Решение 10 задачи в циклах

--- src/CyclesTheme.java
+++ src/CyclesTheme.java
@@ -167,8 +167,17 @@ public class CyclesTheme {
 
         System.out.println("\n10. Вывод таблицы умножения Пифагора");
         for (int i7 = 1; i7 < 10; i7++) {
-            for (int j5 = 1; j5 < 10; j5++) {
-                System.out.printf("%3d", i7 * j5);
+            if (i7 == 1) {
+                System.out.println("  1|  2  3  4  5  6  7  8  9");
+                System.out.print("  -|------------------------");
+            } else {
+                for (int j5 = 1; j5 < 10; j5++) {
+                    if (j5 == 2) {
+                        System.out.printf( "|" +  "%3d", i7 * j5);
+                    } else {
+                        System.out.printf("%3d", i7 * j5);
+                    }
+                }
             }
             System.out.println();
         }
--- src/MyFirstApp.java
+++ src/MyFirstApp.java
@@ -1,6 +1,19 @@
 public class MyFirstApp {
     public static void main(String[] args) {
-        System.out.print("Написано однажды, ");
-        System.out.println("работает везде!");
+        for (int i7 = 1; i7 < 10; i7++) {
+            if (i7 == 1) {
+                System.out.println("  1|  2  3  4  5  6  7  8  9");
+                System.out.print("  -|------------------------");
+            } else {
+                for (int j5 = 1; j5 < 10; j5++) {
+                    if (j5 == 2) {
+                        System.out.printf( "|" +  "%3d", i7 * j5);
+                    } else {
+                        System.out.printf("%3d", i7 * j5);
+                    }
+                }
+            }
+            System.out.println();
+        }
     }
 }
