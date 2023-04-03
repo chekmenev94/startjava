b7ce96eb963cfb13b0e218538e49d3e34381aa6f
chekmenev94 <andrei_94_chekm@mail.ru>
2023-04-03 22:49:34 +0300

    Выполнил работу над ошибками в калькуляторе

--- src/com/startjava/lesson_2_3_4/finaly/CalculatorTest1.java
+++ src/com/startjava/lesson_2_3_4/finaly/CalculatorTest1.java
@@ -1,21 +1,32 @@
 package com.startjava.lesson_2_3_4.finaly;
 
 import java.util.Scanner;
-
 public class CalculatorTest1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String replay;
         do {
-            Calculator1.input();
-            double result = Calculator1.calculate();
-            System.out.printf((result == Math.ceil(result)) ? "Результат вычислений: " +
+            try {
+                System.out.println("Введите операцию: ");
+                String input = scan.nextLine();
+                String[] operation = input.split(" ");
+                double result = Calculator1.calculate(operation);
+                System.out.printf((result == Math.ceil(result)) ? "Результат вычислений: " +
                     Math.round(result): "Результат вычислений: %.3f", result);
-            System.out.println("\nХотите продолжить вычисления? [yes/no] ");
-            replay = scan.nextLine();
-            if (replay.equals ("no")) {
+            } catch(RuntimeException e) {
+                continue;
+            }
+            do {
+                System.out.println("\nХотите продолжить вычисления? [yes/no] ");
+                replay = scan.nextLine();
+                if (!replay.equals ("yes") && !replay.equals ("no")) {
+                    System.out.println("Ответ введен неверно, введите [yes/no]");
+                    replay = scan.nextLine();
+                }
+            } while (!replay.equals ("yes") && !replay.equals ("no"));
+            if (replay.equals("no")) {
                 break;
             }
-        } while(replay.equals ("yes"));
+        } while (true);
     }
 }
