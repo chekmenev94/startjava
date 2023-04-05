867b014b7937e7326d84d2dc692cd09ff4ef0505
chekmenev94 <andrei_94_chekm@mail.ru>
2023-04-05 14:09:41 +0300

    Выполнил работу над ошибками в калькуляторе

--- src/com/startjava/lesson_2_3_4/finaly/Calculator1.java
+++ src/com/startjava/lesson_2_3_4/finaly/Calculator1.java
@@ -1,31 +1,33 @@
 package com.startjava.lesson_2_3_4.finaly;
 
 import java.util.Scanner;
+
 public class Calculator1 {
     static double num1;
-    static String operation1;
+    static String operation;
     static double num2;
 
-    public static double calculate(String[] operation) {
-        num1 = Double.parseDouble(operation[0]);
-        operation1 = operation[1];
-        num2 = Double.parseDouble(operation[2]);
+    public static void input(String input) {
+        String[] expression = input.split(" ");
+        num1 = Integer.parseInt(expression[0]);
+        operation = expression[1];
+        num2 = Integer.parseInt(expression[2]);
         if (num1 < 0 || num2 < 0) {
-            System.err.println("Значения не могут быть отрицательными ");
             throw new RuntimeException();
         } else if (num1 % 1 != 0 || num2 % 1 != 0) {
-            System.err.println("Значения должны быть целыми числами ");
             throw new RuntimeException();
-        } else {
-            return switch (operation1) {
-                case "+" -> num1 + num2;
-                case "-" -> num1 - num2;
-                case "*" -> num1 * num2;
-                case "/" -> num1 / num2;
-                case "%" -> num1 % num2;
-                case "^" -> Math.pow(num1, num2);
-                default -> 0;
-            };
         }
+    }
+
+    public static double calculate() {
+        return switch (operation) {
+            case "+" -> num1 + num2;
+            case "-" -> num1 - num2;
+            case "*" -> num1 * num2;
+            case "/" -> num1 / num2;
+            case "%" -> num1 % num2;
+            case "^" -> Math.pow(num1, num2);
+            default -> 0;
+        };
     }
 }
--- src/com/startjava/lesson_2_3_4/finaly/CalculatorTest1.java
+++ src/com/startjava/lesson_2_3_4/finaly/CalculatorTest1.java
@@ -1,32 +1,34 @@
 package com.startjava.lesson_2_3_4.finaly;
 
 import java.util.Scanner;
+
 public class CalculatorTest1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String replay;
+        String replay = "";
         do {
             try {
                 System.out.println("Введите операцию: ");
                 String input = scan.nextLine();
-                String[] operation = input.split(" ");
-                double result = Calculator1.calculate(operation);
-                System.out.printf((result % 1 == 0) ? "Результат вычислений: " +
-                    Math.round(result): "Результат вычислений: %.3f", result);
+                Calculator1.input(input);
+                double result = Calculator1.calculate();
+                // System.out.printf("Результат вычислений: " + ((result % 1 == 0) ? 
+                //    "%.0f", result : "%.3f",result));
+                if (result % 1 == 0) {
+                    System.out.printf("Результат вычислений: " + "%.0f", result);
+                } else {
+                    System.out.printf("Результат вычислений: " + "%.3f", result);
+                }
             } catch(RuntimeException e) {
+                System.out.println("Значения должны быть положительными и целыми числами");
                 continue;
             }
-            do {
-                System.out.println("\nХотите продолжить вычисления? [yes/no] ");
+            System.out.println("\nХотите продолжить вычисления? [yes/no] ");
+            replay = scan.nextLine();
+            if (!replay.equals ("yes") && !replay.equals ("no")) {
+                System.out.println("Ответ введен неверно, введите [yes/no]");
                 replay = scan.nextLine();
-                if (!replay.equals ("yes") && !replay.equals ("no")) {
-                    System.out.println("Ответ введен неверно, введите [yes/no]");
-                    replay = scan.nextLine();
                 }
-            } while (!replay.equals ("yes") && !replay.equals ("no"));
-            if (replay.equals("no")) {
-                break;
-            }
-        } while (true);
+        } while (!replay.equals ("no"));
     }
 }
